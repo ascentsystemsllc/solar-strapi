5bae2e6070cdbec51113f1cb0adbedfcb5cd3838
Ganesh, Mohan <mohanganesh@gmail.com>
2022-08-31 12:15:22 -0400

    Added cors support- localhost:3000

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,11 @@ module.exports = {
   //....
   settings: {
     cors: {
-      origin: ["http://localhost:1300", "https://solar.mohanganesh.com"],
+      origin: [
+        "http://localhost:1300",
+        "https://solar.mohanganesh.com",
+        "http://localhost:3000",
+      ],
     },
   },
 };
