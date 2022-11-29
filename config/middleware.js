cf9193519a7bc2d7baf5ccc24ac61710f7614361
Ganesh, Mohan <mohanganesh@gmail.com>
2022-11-29 17:35:13 -0500

    Added new ulr for CORS support

--- config/middleware.js
+++ config/middleware.js
@@ -8,7 +8,8 @@ module.exports = {
         "http://localhost:3000",
         "http://localhost:1337",
         "https://solar-strapi.herokuapp.com",
-        "https://strapi-dev-354-2f2ia5goua-uc.a.run.app"
+        "https://strapi-dev-354-2f2ia5goua-uc.a.run.app",
+        "https://restaurant-2f2ia5goua-uc.a.run.app"
       ],
     },
   },
